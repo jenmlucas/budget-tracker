fbe1a36111146ca85c9c081945becb94a7acee34
jenmlucas <jenmlucas1@gmail.com>
2022-01-26 18:45:21 -0700

    corrected routes

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,18 +4,18 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "/",
-  "./index.html",
-  "./css/styles.css",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png",
-  "./manifest.json",
-  "./js/idb.js",
-  "./js/index.js"
+  "/index.html",
+  "/css/styles.css",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/manifest.json",
+  "/js/idb.js",
+  "/js/index.js"
 ];
 
 self.addEventListener("install", function (e) {
